b1f1258872df816e27bb47370f6f79da8ccb166d
Joseph Muruguru <jmuru1@bu.edu>
2015-10-24 02:57:49 -0400

    only gulp tasks so far is browser-sync

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express(); 
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'})); 
+app.set('view engine', 'handlebars'); 
+
+var index_page = require('./routes/index.js');
+
+
+app.use('/', index_page); 
+app.use(express.static('public')); 
+
+
+
+app.listen(3000, function() {
+	console.log('listenig on port 3000.... ');
+});
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require("gulp");
+var browserSync = require("browser-sync");
+var nodemon = require('gulp-nodemon');
+
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync({
+    proxy: "localhost:3000",  // local node app address
+    port: 5000,  // use *different* port than above
+    notify: true
+  });
+});
+
+gulp.task('nodemon', function (cb) {
+  var called = false;
+  nodemon({
+    script: 'app.js',
+    ignore: [
+      'gulpfile.js',
+      'node_modules/'
+    ]
+  })
+  .on('start', function () {
+    if (!called) {
+      called = true;
+      cb();
+    }
+  })
+  .on('restart', function () {
+    setTimeout(function () {
+      browserSync.reload();
+    }, 1000);
+  });
+});
+
+gulp.task('default', ['browser-sync'], function () {
+  gulp.watch(['./views/*.handlebars'], browserSync.reload);
+});
